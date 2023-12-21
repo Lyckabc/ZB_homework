fb02f298b0b4d8f30df341e03beda01dbc27cc59
Lyckabc <dh2heart@gmail.com>
2023-12-22 08:36:22 +0900

    04 Hash programmers fini

--- ZBStr/ZBStr_04_Hash/P42578_FashionLeader/P42578.java
+++ ZBStr/ZBStr_04_Hash/P42578_FashionLeader/P42578.java
@@ -2,24 +2,30 @@ package P42578;
 
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.*;
 
 public class P42578 {
     public static void main(String[] args) {
-
+        Solution sol = new Solution();
+        String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        System.out.println(sol.solution(clothes));
     }
 }
 
 class Solution {
     public int solution(String[][] clothes) {
-        HashMap<String, List<String>> map = new HashMap<String, List<String>>();
-        int answer = 0;
+        HashMap<String, Integer> map = new HashMap<>();
+        int answer = 1;
         for (int i = 0; i < clothes.length; i++) {
-            if (!map.containsKey(clothes[i][1]) ) {
-                map.put(clothes[i][1], new List<>());
-            }
-            map.get(clothes[i][1]).add(clothes[i][0]);
-
+            String type = clothes[i][1];
+            map.put(type, map.getOrDefault(type, 0) + 1);
         }
-        return answer;
+        Iterator<Integer> iter = map.values().iterator();
+
+        while (iter.hasNext()) {
+            answer *= iter.next().intValue() + 1;
+        }
+
+        return answer - 1;
     }
 }
